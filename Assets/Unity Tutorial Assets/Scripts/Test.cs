7a6a43bdccd5eae8b189b5537f43c9e8420b195a
Joanna <joanna@Joannas-MacBook-Air.local>
2016-02-08 11:18:30 -0800

    Added test script

---
+++ Assets/Unity Tutorial Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hi");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
