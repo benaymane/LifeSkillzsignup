21d90aee6767eb6c496fedff00eee828d1bdb185
connor-a-smith <c3smith@ucsd.edu>
2016-02-17 21:26:14 -0800

    Added check game

--- Assets/Check Minigame/Scripts/CheckSlots.cs
+++ Assets/Check Minigame/Scripts/CheckSlots.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Test : MonoBehaviour {
+public class CheckSlots : MonoBehaviour {
+
+    public int thisSlotIndex;
 
 	// Use this for initialization
 	void Start () {
-
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public int getIndex() {
+
+	  return thisSlotIndex;
+
 	}
 }
---
+++ Assets/Check Minigame/Scripts/DraggablePieces.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DraggablePieces : MonoBehaviour {
+
+    public int thisPieceIndex;
+    private bool draggable = true;
+
+    private Vector3 mouseOffset;
+    private Vector3 currPos;
+
+	// Use this for initialization
+	void Start () {
+
+	  currPos = this.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	  //when the button is first pressed down
+	  if (Input.GetMouseButtonDown(0)) {
+
+	    //set the mouse offset from the center of the game object
+	    mouseOffset = currPos - Camera.main.ScreenToWorldPoint(
+	      new Vector3(Input.mousePosition.x,
+	                  Input.mousePosition.y,
+	                  Input.mousePosition.z));
+
+
+	  }
+
+	  //while the mouse button is being held down
+	  if (Input.GetMouseButton(0)) {
+
+	     //checks if the user is pressing on the draggable object
+	     RaycastHit2D hit = Physics2D.Raycast(
+	       Camera.main.ScreenToWorldPoint(Input.mousePosition), -Vector2.up);
+
+	     //if object is being pressed
+	     if (hit && hit.collider.GetComponent<DraggablePieces>() == this) {
+
+	      //if the object can currently be dragged
+	      if (draggable) {
+
+	        //calculates location where object should be dragged
+	        Vector2 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + mouseOffset;
+
+	        //sets current object's position
+            this.transform.position = newPosition;
+
+            //updates current object position
+	        currPos = this.transform.position;
+
+	      }
+	    }
+	  }
+
+	  else {
+
+	    mouseOffset = currPos - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+	                                                                       Input.mousePosition.y,
+	                                                                       Input.mousePosition.z));
+
+	  }
+	}
+
+	/*
+	 * If the dragged object is within a check slot
+	 */
+	void OnTriggerStay2D (Collider2D slot) {
+
+	  //variable for the slot that the object was dragged to
+	  CheckSlots draggedSlot;
+
+	  //sets the correct slot
+      draggedSlot = slot.gameObject.GetComponent<CheckSlots>();
+
+      //if the object was dragged to a slot
+	  if (draggedSlot) {
+
+	    //if the mouse button is not still being pressed
+	    if (!Input.GetMouseButton(0)) {
+
+	      //if the index of the slot matches the index of the piece
+	      if (draggedSlot.getIndex() == thisPieceIndex) {
+
+	        //snap the check piece into place
+	        Snap(draggedSlot.gameObject.transform.position);
+
+	      }
+	    }
+	  }
+	}
+
+	/*
+	 * Snaps an object to a location
+	 */
+	void Snap(Vector2 location) {
+
+	  //sets position
+	  this.transform.position = location;
+
+	  //makes object impossible to drag anymore.
+	  draggable = false;
+
+	}
+}
