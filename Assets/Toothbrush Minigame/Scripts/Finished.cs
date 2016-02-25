1780088d9b63db86541b2e42d85abe132dfc7560
airokay <airokairok@gmail.com>
2016-02-24 23:00:34 -0800

    Toothbrush Functional

---
+++ Assets/Toothbrush Minigame/Scripts/Finished.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finished : MonoBehaviour {
+    public GameObject teethMom;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void finish(){
+        bool clean = true;
+        foreach(Transform tooth in teethMom.transform)
+        {
+            if (tooth.gameObject.GetComponent<Teeth>().touched == false)
+                clean = false;
+        }
+
+        if (clean)
+            Debug.Log("CLEAN!");
+        else
+            Debug.Log("Dirty!! :C");
+
+       
+        
+    }
+
+}
