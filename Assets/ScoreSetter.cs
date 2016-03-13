282be9e6269c8f8697c6c2155a028bed976fb00f
Kristin Agcaoili <kagcaoil@ucsd.edu>
2016-03-13 00:37:54 -0800

    Added score keeper

---
+++ Assets/ScoreSetter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreSetter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text> ().text = "Score: " + GlobalScore.score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
