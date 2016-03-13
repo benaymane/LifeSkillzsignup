b14f496921be1b0844f6d59e51bce9ff9df55392
Kristin Agcaoili <kagcaoil@ucsd.edu>
2016-03-13 04:24:41 -0700

    Started placing art for toothbrush game. Slight functionality for brushing.

--- Assets/Toothbrush Minigame/Scripts/Teeth.cs
+++ Assets/Toothbrush Minigame/Scripts/Teeth.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class Teeth : MonoBehaviour {
 
     public bool touched = false;
-
+	public GameObject toothbrush;
+	public float maxRange = 0f;
+	public float minRange = -1f;
 
 
 	// Use this for initialization
@@ -14,11 +16,15 @@ public class Teeth : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector3 vecCollide = toothbrush.GetComponent<CircleCollider2D> ().transform.position - GetComponent<BoxCollider2D> ().transform.position;
+		if (vecCollide.y < maxRange && vecCollide.y > minRange) {
+			Debug.Log ("clean clean clean");
+		}
+		//Debug.Log (vecCollide);
 	}
 
     void OnTriggerEnter2D (Collider2D other) {
         touched = true;
-
+		Debug.Log ("teeth");
     }
 }
