361c281a8dc6625e2ac76e42da66cbaaa926068e
benaymane <ben.aymane@gmail.com>
2016-11-20 04:17:16 -0800

    Added Login page buttons functionality to be able to change scenes, no input (username, pass) required.

---
+++ Assets/Login/Login.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Login : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void signupTransfer( ) {
+        SceneManager.LoadScene("Register");
+    }
+
+    public void overworldTransfer() {
+        SceneManager.LoadScene("OverviewMap");
+    }
+}
